4917f85f6619b1ca70b17f59813720c5230c0837
ravi-d-parikh <136847291+ravi-d-parikh@users.noreply.github.com>
2023-06-17 00:17:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello we are Prosupplyr %s' % (self.path)
+        msg = 'Hello we are Prosupplyr and this is our Logistics Planning Website %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
